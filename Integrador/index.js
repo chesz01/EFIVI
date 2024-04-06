d23223356584b20a3d9cc63eb94696a51fe9b48d
chesz01 <eliplay91@gmail.com>
2024-04-06 18:22:56 -0300

    upload de arquivos
    1

---
+++ Integrador/index.js
@@ -0,0 +1,11 @@
+const hamButton = document.getElementById('hamb');
+const menu = document.querySelector('.menu');
+const hamburg = document.querySelector('.h-img');
+const x = document.querySelector('.x-img');
+
+hamButton.addEventListener('click', function() {
+    menu.classList.toggle('show');
+
+    hamburg.classList.toggle('hiden');
+    x.classList.toggle('hiden');
+});
