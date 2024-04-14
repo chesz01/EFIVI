705fd4cb6a54833949e4ad3b5cd64ef194f2dd5c
chesz01 <eliplay91@gmail.com>
2024-04-14 18:52:43 -0300

    1404

--- Integrador/index.js
+++ Integrador/index.js
@@ -11,7 +11,16 @@ hamButton.addEventListener('click', function() {
     x.classList.toggle('hiden');
 });
 
-//=======================================================
+//=======================================================DUVIDAS
+
+perg_resp = {
+    "Onde se localiza a ong EFIVI ?": "Rua Octávio Alves de Araujo, 68 - Tarraf II - Catanduva - SP",
+    "O que a EFIVI faz ?": "Reciclamos material eletro/eletrônico, montamos e doamos computadores adaptados para deficientes visuais e lecionamos cursos de eletrônica totalmente gratuito em nossa cede.",
+    "Qual o propósito da ong EFIVI ?": "Impactar positivamente o meio ambiente e integrar o deficiente visual no mundo digital.",
+    "Como a ong se mantem ?": "Componentes que não podem ser recuperados são desmontados e vendidos.",
+    "QUais principais atividades da ong ?": "Coleta, manutenção e doação de eletrônicos, suporte para entidades parceiras e disponibilização de cursos.",
+    "Qual o impacto da reciclaem de eletrônicos ?": "aucelora",
+}
 
 //=======================================================
 
